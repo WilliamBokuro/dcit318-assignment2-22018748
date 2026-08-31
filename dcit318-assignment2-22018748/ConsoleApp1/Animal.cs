41aa91b6ccaa2d3f8003b3015f66ce4db8c754ad
William Bokuro <wbokuro@st.ug.edu.gh>
2026-08-30 12:20:23 -1200

    Inheritance and Method Overriding

---
+++ dcit318-assignment2-22018748/ConsoleApp1/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+    // Base Class
+    public class Animal
+    {
+        // Virtual method allows derived classes to override it
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived Class: Dog
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived Class: Cat
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class InheritanceDemo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("--- Task 1: Inheritance & Method Overriding ---");
+
+            Animal genericAnimal = new Animal();
+            Dog myDog = new Dog();
+            Cat myCat = new Cat();
+
+            genericAnimal.MakeSound();
+            myDog.MakeSound();
+            myCat.MakeSound();
+
+            Console.WriteLine();
+        }
+    }
+
