7630f736b5116807dd56a9f8a4131845b03f467c
William Bokuro <wbokuro@st.ug.edu.gh>
2026-08-30 14:51:37 -1200

    Interfaces

---
+++ dcit318-assignment2-22018748/ConsoleApp1/IMovable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+    // Interface Definition
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    // Class implementing IMovable
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Another class implementing IMovable
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class InterfaceDemo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("--- Task 3: Interfaces ---");
+
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            myCar.Move();
+            myBicycle.Move();
+
+            Console.WriteLine();
+        }
+    }
