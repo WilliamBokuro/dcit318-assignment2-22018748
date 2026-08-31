cd50a6fb4396ed52ab11a4c0f891d47b9ed844da
William Bokuro <wbokuro@st.ug.edu.gh>
2026-08-30 20:19:01 -1200

    Main Point Entry

---
+++ dcit318-assignment2-22018748/ConsoleApp1/Mainpoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Execute all three tasks in order
+            InheritanceDemo.Run();
+            AbstractClassDemo.Run();
+            InterfaceDemo.Run();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
